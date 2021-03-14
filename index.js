53c1c3659b5d574f245793a0ec261dd0131f38b8
Samantha <samanthademotta@yahoo.com>
2021-03-13 21:37:20 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,54 @@
+var number=1000
+var number=100
+
+function add() {
+    return number+=5;
+}
+function subtract() {
+    return number-=5;
+}
+function multiply() {
+    return number*=5;
+}
+function divide() {
+    return number/=5;
+}
+function add(number,number2){
+    return number += number2;
+}
+function subtract(number, number2) {
+    return number -= number2;
+}
+function multiply(number, number2) {
+    return number *= number2;
+}
+function divide(number, number2) {
+    return number /= number2;
+}
+function increment(n) {
+    return n+=1;
+}
+function decrement(n) {
+    return n-=1;
+}
+function makeInt(n) {
+    parseInt(`10`);
+    return parseFloat;
+}
+function preserveDecimal(n) {
+    parseFloat();
+    return parseFloat;
+}
+function roughScale(x, base) {
+    return parsed * 100;
+   }
+function makeInt(n){                                                                                                                                                              let base=10;                                                                                                                                                                                                        return parseInt(n,base);                                                                                            }                                                                                             function preserveDecimal(n){
+     return   parseFloat(n);
+}
+function makeInt(n){   
+    let base=10;    
+    return parseInt(n,base);
+}
+function preserveDecimal(n){
+    return   parseFloat(n);
+}
